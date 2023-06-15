bca5775cfb974a86bcb132ae8e70a6173a2729a4
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-06-15 22:41:31 +0300

    first commit

---
+++ my_enumerable.rb
@@ -0,0 +1,14 @@
+module MyEnumerable
+    def all?(&block)
+      @list.all?(&block)
+    end
+  
+    def any?(&block)
+      @list.any?(&block)
+    end
+  
+    def filter(&block)
+      @list.select(&block)
+    end
+  end
+  
